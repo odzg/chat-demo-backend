5ac56cfb9c5a5bb42a8ee1a8a80858a3babaa6c7
Oded Zilbershlag <123odzil@gmail.com>
2024-11-24 17:52:21 +0200

    Changed value of "@typescript-eslint/consistent-type-definitions" to default, which is 'interface'

--- fastify-jwt.d.ts
+++ fastify-jwt.d.ts
@@ -5,7 +5,6 @@ import type { User } from '@/schemas/user-schemas.ts';
 export type {} from '@fastify/jwt';
 
 declare module '@fastify/jwt' {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface FastifyJWT {
     payload: Simplify<Pick<User, 'id'>>;
     user: Simplify<Pick<User, 'id'>>;
